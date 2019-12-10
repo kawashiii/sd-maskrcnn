7c7f82b74f79c1450a906fe55c98dbd9419abbea
kawashiii <turquoise12-1@hotmail.co.jp>
2019-12-10 05:31:10 +0000

    fix pre-trained model to install necessarily

--- install.sh
+++ install.sh
@@ -16,21 +16,21 @@ else
 fi
 
 # Download pretrained model if requested
-shopt -s nocasematch
-unset response
-while [[ ! $response =~ (y|yes|n|no) ]]; do
-    echo -n "Download Pre-trained Model to models/ (150 MB)? (y/n) > "
-    read response
-done
+# shopt -s nocasematch
+# unset response
+# while [[ ! $response =~ (y|yes|n|no) ]]; do
+#     echo -n "Download Pre-trained Model to models/ (150 MB)? (y/n) > "
+#     read response
+# done
 
-case "$response" in
-    [yY][eE][sS]|[yY]) 
-        mkdir -p models
-        wget -v -O models/sd_maskrcnn.h5 https://berkeley.box.com/shared/static/obj0b2o589gc1odr2jwkx4qjbep11t0o.h5
-        ;;
-    *)
-        ;;
-esac
+# case "$response" in
+#     [yY][eE][sS]|[yY]) 
+mkdir -p models
+wget -v -O models/sd_maskrcnn.h5 https://berkeley.box.com/shared/static/obj0b2o589gc1odr2jwkx4qjbep11t0o.h5
+#         ;;
+#     *)
+#         ;;
+# esac
 
 if [ "$#" == "1" ] && [ "$1" == "generation" ]
 then
